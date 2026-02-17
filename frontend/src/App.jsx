0a2107864c3cfd7e5c3318adf8af0bee45c332bd
Sahil-Thakur-31 <sahilthakur3109@gmail.com>
2026-02-17 19:26:11 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,18 @@ import Sidebar from "./components/sideBar";
 import Profile from './pages/profile';
 import Settings from './pages/Settings';
 
+const dummyUser = {
+    name: "Rahul Sharma",
+    role: "admin" // try: "sales-person", "sales-manager"
+};
+
+
 function App() {
   return (
         <div className="app-root">
           <Sidebar />
           <div className="main-container">
-            <Profile/>
+            <Profile />
           </div>
         </div>
     );
