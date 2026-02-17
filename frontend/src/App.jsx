bafcb0e6325af9d6994e72584dd809b881a0d4b0
Sahil-Thakur-31 <sahilthakur3109@gmail.com>
2026-02-17 16:07:29 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import './App.css'
 import Sidebar from "./components/sideBar";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div>Main Content</div>
+      </div>
+    );
 }
 
 export default App
+
