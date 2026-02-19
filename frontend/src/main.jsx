f202537dc495ad043facb48119807030e2af6b1c
Sahil Thakur <sahilthakur3109@gmail.com>
2026-02-19 12:49:49 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css';
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
