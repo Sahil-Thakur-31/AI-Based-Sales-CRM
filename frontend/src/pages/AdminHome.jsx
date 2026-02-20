f68a5db6fd82cfd4c2ade2423721bce3d69cf2a6
Sahil Thakur <sahilthakur3109@gmail.com>
2026-02-20 19:16:10 +0530

    .

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -1,32 +1,3 @@
-// import {useState,useEffect} from 'react'
-// import {useNavigate}  from 'react-router-dom'
-
-// function AdminHome(){
-//     const [loggedinUser,setLoggedinUser] = useState('');
-//     const navigate = useNavigate();
-//     useEffect(()=>{
-//         setLoggedinUser(localStorage.getItem('Name'));
-//     },[]);
-
-//     const handleLogout = () =>{
-//         localStorage.removeItem('token');
-//         localStorage.removeItem('Name');
-//         setTimeout(()=>{
-//             navigate('/login');
-//         },100);
-//     }
-
-//     return(
-//         <div>
-//             <p>{loggedinUser}</p>
-//             <a href='/manageUsers'>Users</a>
-//             <button type='submit' onClick={handleLogout}>Logout</button>
-//         </div>
-//     )
-// }
-
-// export default AdminHome;
-
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../styles/AdminHome.css";
