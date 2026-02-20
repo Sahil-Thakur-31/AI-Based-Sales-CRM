71b03d1d7bba0f9ded561a67bd0bbf14141bde13
Sahil Thakur <sahilthakur3109@gmail.com>
2026-02-20 11:38:43 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,19 +3,8 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css';
 
-<<<<<<< HEAD
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
-
-
-=======
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-);
->>>>>>> f64418250e868264979133c4695e76d103a8af45
