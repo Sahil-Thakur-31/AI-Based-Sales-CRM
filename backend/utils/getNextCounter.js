2669f8ca82d2ea13c970810b09638f98252b472b
Omkar <kambleomkar440@gmail.com>
2026-02-24 19:31:36 +0530

    .

--- backend/utils/getNextCounter.js
+++ backend/utils/getNextCounter.js
@@ -2,12 +2,9 @@ const Counter = require("../models/counter");
 
 const getNextCounter = async (counterType) => {
   const counter = await Counter.findOneAndUpdate(
-    // { counterType: counterType.toUpperCase() }, // match your schema
-    // { $inc: { value: 1 } },                     // increment correct field
-    // { new: true, upsert: true }
-    // { name },
-    // { $inc: { seq: 1 } },
-    // { returnDocument: "after", upsert: true }
+    { counterType: counterType.toUpperCase() }, // match your schema
+    { $inc: { value: 1 } },                     // increment correct field
+    { new: true, upsert: true }
   );
 
   return counter.value; // return updated number
