84104dc701addad84f9e7f251064bb9fa4a4516c
ambadas19 <deshmukhambadas510@gmail.com>
2026-03-01 14:50:18 +0530

    .

--- backend/routes/dealsRoutes.js
+++ backend/routes/dealsRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authenticate = require("../middlewares/auth");
 const { getDeals, getDealById, updateDeal, deleteDeal, restoreDeal } = require("../controllers/dealsController");
 
-router.get("/client-suggestions", authenticate, getClientSuggestions);
+// router.get("/client-suggestions", authenticate, getClientSuggestions);
 router.get("/", authenticate, getDeals);
 router.get("/:id", authenticate, getDealById);
 router.put("/:id/restore", authenticate, restoreDeal);
